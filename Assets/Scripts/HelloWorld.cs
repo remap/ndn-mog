c4a29cfb46b2ffa1d78ae864812c0fa4603054cf
Zening <quzening@gmail.com>
2012-10-01 10:12:00 +0800

    hello world

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Hello World! This is Little Prince!");
+	}
+	
+	
+}
