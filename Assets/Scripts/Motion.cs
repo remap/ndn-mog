6f96ec7ba3c78c98e6c2bd6d1ef348e1569e901f
Zening <quzening@gmail.com>
2013-01-30 10:27:34 +0800

    Enhanced Motion Control;

--- Assets/Scripts/Motion.cs
+++ Assets/Scripts/Motion.cs
@@ -3,10 +3,35 @@ using System.Collections;
 
 public class Motion : MonoBehaviour {
 
-	public float speed = 90f;
+	public float rotate_speed = 90f; // for rotating around 
+	public float zoom_speed = 90f; // for zooming
+	public float min_bound = 60f;
+	public float max_bound = 300f;
 	
 	void Update () {
-		float movement = Input.GetAxis("Horizontal") * speed;
-		transform.RotateAround (Vector3.zero, Vector3.up, (-1) * movement * Time.deltaTime);
+		float rotate_movement = Input.GetAxis("Horizontal") * rotate_speed;
+		transform.RotateAround (Vector3.zero, Vector3.up, (-1) * rotate_movement * Time.deltaTime);
+		float zoom_movement = Input.GetAxis("Vertical") * zoom_speed;
+		if(SafeInBound(zoom_movement)==true)
+		{
+			transform.Translate(Vector3.forward * zoom_movement * Time.deltaTime);
+		}
+		
+		
+	}
+	
+	bool SafeInBound(float movement)
+	{
+		bool safe = true;
+		float dis = Vector3.Distance(Vector3.zero, this.transform.position);
+		if(dis<=min_bound && movement>0)
+		{
+			safe=false;
+		}
+		else if(dis>=max_bound && movement<0)
+		{
+			safe=false;
+		}
+		return safe;
 	}
 }
