cc35801da657e3b24991570bfdd47333d671a28a
Zening <quzening@gmail.com>
2013-07-01 20:00:22 -0700

    Added GUI Text; I need another script to control the labels;

---
+++ Assets/Scripts/ObjectLabel.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+ 
+[RequireComponent (typeof (GUIText))]
+public class ObjectLabel : MonoBehaviour {
+ 
+public Transform target;  // Object that this label should follow
+public static Vector3 offset = new Vector3(0f, 1.5f, 0f);    // Units in world space to offset; 1 unit above object by default
+
+public Camera cameraToUse ;   // Only use this if useMainCamera is false
+
+ 
+    void Update()
+    {
+    	transform.position = cameraToUse.WorldToViewportPoint(target.position + offset);
+    }
+}
